54e783d87f3546b7464c03ad2176b0ba21e77388
Иван Валеев <dp171190vii1@gmail.com>
2025-08-13 18:41:08 +0300

    create site

---
+++ app.py
@@ -0,0 +1,20 @@
+﻿from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    
+    info = {
+        "name": "Ірина Швидка",
+        "email": "test@gmail.com",
+        "phone": "+380 ** *** ***",
+        "about": "Цей сайт присвячено котячому розпліднику Ediscaprise.",
+        "facebook": "facebook",
+        "instagram": "instagram"
+        }
+    
+    return render_template("index.html", info=info)
+
+if __name__ == "__main__":
+    app.run(debug=True)
